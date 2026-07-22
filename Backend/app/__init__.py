48dffda506b3c7428af71618726d85740b967f89
DHRUBOJYOTI BHATTACHARJEE <117100651+dhrubooou@users.noreply.github.com>
2026-07-23 03:45:25 +0530

    Migration feature added

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from app.config import Config
 from app.extensions import get_db_connection as db
+from app.migrations import run_migrations
 
 def create_app():
     app=Flask(__name__)
     app.config.from_object(Config)
+    run_migrations(app)
+
     
     @app.route("/")
     def home():
